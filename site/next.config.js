37f3576309d2676f1df3150e2535f3996b431b32
fabriciobuttbutt <70394536+fabriciobuttbutt@users.noreply.github.com>
2023-06-26 14:50:22 -0300

    Update next.config.js
    primeiro commit

--- site/next.config.js
+++ site/next.config.js
@@ -2,11 +2,11 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'Butt Kasm Registry',
+    description: 'Butt s official workspaces.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    listUrl: 'https://fabriciobuttbutt.github.io/kasm-registry/',
+    contactUrl: 'https://github.com/fabriciobuttbutt/kasm-registry/issues',
   },
   reactStrictMode: true,
   swcMinify: true,
